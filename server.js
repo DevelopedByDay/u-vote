5323ef4ea4372db8819a04b038376f1196280381
BradD <bradleyeday@gmail.com>
2020-08-12 16:05:50 -0500

    add postroute

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -63,13 +64,6 @@ app.delete('/api/candidate/:id', (req, res) => {
     });
   });
 
-// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result, this, this.changes);
-// });
-
 // const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
 //                 VALUES (?,?,?,?)`;
 // const params = [1, 'ROnald', 'Firbank', 1];
@@ -80,6 +74,32 @@ app.delete('/api/candidate/:id', (req, res) => {
 //     }
 //     console.log(result, this.lastID);
 // });
+
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.run(sql, params, function(err, result) {
+    if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+    }
+
+    res.json({
+        message: 'success',
+        data: body,
+        id: this.lastID
+    });
+    });
+    
+});
 
 app.use((req,res) => {
     res.status(404).end();
